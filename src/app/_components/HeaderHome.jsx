a256a1bcabdf2b1231cd048cae38ee28a5d87756
Abel Mauricio Quiroz Mora <abelquiroz38@gmail.com>
2026-03-30 14:27:18 -0600

    logo puesto

--- src/app/_components/HeaderHome.jsx
+++ src/app/_components/HeaderHome.jsx
@@ -3,7 +3,7 @@ import MenuHome from "./MenuHome";
 export default function HeaderHome() {
   return (
     <header className="w-max">
-      <div className="w-[90%] m-auto">
+      <div className="w-[90%] m-auto flex justify-around">
         <Logo />
         <MenuHome />
       </div>
--- src/app/_components/Logo.jsx
+++ src/app/_components/Logo.jsx
@@ -1,3 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Logo() {
-  return;
+  return (
+    <Link href="/">
+      <Image
+        src="/img/logo vectorizado.svg"
+        alt="logo milas"
+        width="80"
+        height="80"
+        className="w-auto h-10  md:h-6 "
+      />
+    </Link>
+  );
 }
